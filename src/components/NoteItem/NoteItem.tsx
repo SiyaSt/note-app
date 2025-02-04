64d9f9ec9ae393b1b999fd9bd59ebea2a43b1c90
oooh_shit <anastasiya.stecenko@gmail.com>
2025-02-04 16:43:54 +0300

    feat: add logic with useState and props

--- src/components/NoteItem/NoteItem.tsx
+++ src/components/NoteItem/NoteItem.tsx
@@ -1,23 +1,47 @@
 import { Button, Card } from "react-bootstrap";
 import "./NoteItem.scss";
 import { CustomModal } from "components/CustomModal/CustomModal.tsx";
-import { useState } from "react";
+import { FC, useState } from "react";
+import { Note } from "shared/types/note.ts";
 
-export const NoteItem = () => {
+interface NoteItemProps {
+  note: Note;
+  setIsEditing: (IsEditing: boolean) => void;
+  onDeleteNote: () => void;
+}
+
+export const NoteItem: FC<NoteItemProps> = ({
+  note,
+  onDeleteNote,
+  setIsEditing,
+}) => {
   const [show, setShow] = useState(false);
 
   return (
     <Card>
       <Card.Header>
-        <h3></h3>
+        <h3>{note.title}</h3>
       </Card.Header>
       <Card.Body className="note-item-body">
-        <small className="text-muted last-edited">Last edited: {}</small>
+        <small className="text-muted last-edited">
+          Last edited: {note.date}
+        </small>
+        <p>{note.description}</p>
         <div className="button-group">
-          <Button variant="danger" className="delete-button">
+          <Button
+            variant="danger"
+            className="delete-button"
+            onClick={() => setShow(true)}
+          >
             Delete Note
           </Button>
-          <Button variant="success" className="edit-button">
+          <Button
+            variant="success"
+            className="edit-button"
+            onClick={() => {
+              setIsEditing(true);
+            }}
+          >
             Edit Note
           </Button>
           <CustomModal
@@ -25,6 +49,7 @@ export const NoteItem = () => {
             description="Are you sure you want to delet note?"
             show={show}
             setShow={setShow}
+            onClick={onDeleteNote}
           />
         </div>
       </Card.Body>
